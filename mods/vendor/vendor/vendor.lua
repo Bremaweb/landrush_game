b006e6457e9ab7fe7f5a5e07809f77f654f2f2ab
placki <nobimp@gmail.com>
2016-09-29 11:34:15 +0200

    fix vendor shops reset on edit abort

--- mods/vendor/vendor/vendor.lua
+++ mods/vendor/vendor/vendor.lua
@@ -87,6 +87,10 @@ vendor.can_dig = function(pos,player)
 end
 
 vendor.on_receive_fields = function(pos, formname, fields, sender)
+	-- print("Vendor at "..minetest.pos_to_string(pos).." got "..dump(fields))
+	if ( fields.number == nil or fields.limit == nil or fields.cost == nil or fields.shop == nil ) then -- invalid/aborted form
+		return
+	end
 	local node = minetest.env:get_node(pos)
 	local description = minetest.registered_nodes[node.name].description;
 	local meta = minetest.env:get_meta(pos)
