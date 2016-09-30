1bf93599c1b0fe20191e339b2bac5ce91754a0a7
Brandon <brandon@bremaweb.com>
2016-09-30 13:19:37 -0500

    More fixing of missing starsplus nodes

--- mods/override/stairsplus.lua
+++ mods/override/stairsplus.lua
@@ -15,4 +15,58 @@ stairsplus.legacy_register_stair_and_slab_and_panel_and_micro("wood", "default:w
 		"Wooden Panel",
 		"Wooden Microblock",
 		"wood")
+
+stairsplus.legacy_register_stair_and_slab_and_panel_and_micro("steelblock", "default:steelblock",
+		{snappy=1,bendy=2,cracky=1,melty=2,level=2},
+		{"default_steel_block.png"},
+		"Steel Block Stairs",
+		"Steel Block Slab",
+		"Steel Block Panel",
+		"Steel Microblock",
+		"steelblock")
+		
+stairsplus.legacy_register_stair_and_slab_and_panel_and_micro("desert_stone", "default:desert_stone",
+		{cracky=3},
+		{"default_desert_stone.png"},
+		"Desert Stone Stairs",
+		"Desert Stone Slab",
+		"Desert Stone Panel",
+		"Desert Stone Microblock",
+		"desert_stone")
+		
+stairsplus.legacy_register_stair_and_slab_and_panel_and_micro("glass", "default:glass",
+		{snappy=2,cracky=3,oddly_breakable_by_hand=3},
+		{"default_glass.png"},
+		"Glass Stairs",
+		"Glass Slab",
+		"Glass Panel",
+		"Glass Microblock",
+		"glass")
+		
+stairsplus.legacy_register_stair_and_slab_and_panel_and_micro("cobble", "default:cobble",
+		{cracky=3},
+		{"default_cobble.png"},
+		"Cobblestone Stairs",
+		"Cobblestone Slab",
+		"Cobblestone Panel",
+		"Cobblestone Microblock",
+		"cobble")
+		
+stairsplus.legacy_register_stair_and_slab_and_panel_and_micro("mossycobble", "default:mossycobble",
+		{cracky=3},
+		{"default_mossycobble.png"},
+		"Mossy Cobblestone Stairs",
+		"Mossy Cobblestone Slab",
+		"Mossy Cobblestone Panel",
+		"Mossy Cobblestone Microblock",
+		"mossycobble")
+
+stairsplus.legacy_register_stair_and_slab_and_panel_and_micro("brick", "default:brick",
+		{cracky=3},
+		{"default_brick.png"},
+		"Brick Stairs",
+		"Brick Slab",
+		"Brick Panel",
+		"Brick Microblock",
+		"brick")
 		
